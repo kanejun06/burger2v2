6571ca97abbbef1d01b9a088cde2b7c12ecc5341
kanejun06 <60838183+kanejun06@users.noreply.github.com>
2020-03-05 17:28:57 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,174 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.baDing.play()
+    info.changeScoreBy(1)
+    mySprite4.setPosition(Math.randomRange(10, 150), Math.randomRange(10, 110))
+})
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+	
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    mySprite3.say("ouch!", 400)
+    mySprite3.startEffect(effects.fountain, 400)
+    music.pewPew.play()
+    info.changeLifeBy(-1)
+    pause(1500)
+})
+info.onLifeZero(function () {
+    music.stopAllSounds()
+    pause(1500)
+    game.over(false, effects.hearts)
+})
+let Enemy3: Sprite = null
+let enemyGenFlag = 0
+let mySprite4: Sprite = null
+let mySprite3: Sprite = null
+scene.setBackgroundColor(12)
+let Enemy1 = sprites.create(img`
+. . . . . . . . . . . c c c c c 6 6 6 6 6 . . . . . . . . . . . 
+. . . . . . . . c c c 4 4 4 4 4 4 4 4 4 4 6 6 6 . . . . . . . . 
+. . . . . . c c 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 6 6 . . . . . . 
+. . . . . c b 4 4 4 4 b b 4 4 4 4 b 5 b 4 4 4 4 4 4 b . . . . . 
+. . . . e b 4 4 4 4 b 5 b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 b . . . . 
+. . . e b b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 6 . . . 
+. . e b 6 b b 4 4 4 4 4 4 4 4 4 b b 4 4 4 b 5 b 4 4 4 4 4 6 . . 
+. . e 6 b b 5 b 4 4 4 4 4 4 4 4 b 5 b 4 4 4 b 4 4 b b 4 4 e . . 
+. e 6 6 b 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 4 4 b 5 b 4 4 e . 
+. e 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 e . 
+e b 6 6 b 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b e 
+e b 6 6 b b 4 4 4 b 5 b 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 b e 
+f b 6 6 6 b 4 4 4 b b 4 4 4 4 4 4 4 4 4 b 5 b 4 4 4 4 4 4 4 b f 
+f c b 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 b c f 
+. f b b 6 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f . 
+. e f b b 6 6 6 6 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f e . 
+. 8 6 f c b b 6 6 6 6 6 b b b 4 4 4 4 4 4 4 4 4 4 4 b c c 6 8 8 
+8 7 7 2 e f f c b b b b b b b b b b b b b b b b c f c 2 2 7 7 8 
+8 7 7 2 2 2 2 2 c c c c c c c c c c c c c c c c 2 2 2 2 6 6 7 8 
+f 8 6 6 6 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 7 6 6 8 6 f 
+f e f 8 6 6 6 7 7 7 6 6 6 6 7 7 7 7 7 7 6 6 6 7 7 7 7 f f f e f 
+f b f f 8 7 7 7 6 8 f 8 6 7 7 7 7 7 7 6 6 6 7 7 6 f f f f f b f 
+f b e f f e e f f f e f f 7 7 6 6 6 8 8 e f f e e e e f e b 6 f 
+f 6 b f f f e f f e e e e e e e e e e e e e f e e e e e b b 6 e 
+f 6 6 d d f f f f f e e e f f e f f e e e e e f f e e d b 4 6 e 
+. c 6 6 d d d 4 e f f f f f f e e e e e f f f f 4 d d b 4 6 e . 
+. f c 6 b 4 d d d d d d d d d d d d d d d d d d d b 4 4 4 e e . 
+. . f f 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . 
+. . . . f f b b b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . . . 
+. . . . . . f f e b b b b b b 4 4 4 4 4 4 4 4 e e e . . . . . . 
+. . . . . . . . . f f f f f f f c c c c c e e . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+Enemy1.vx = 40
+Enemy1.vy = 60
+Enemy1.setFlag(SpriteFlag.BounceOnWall, true)
+let Enemy2 = sprites.create(img`
+. . . . . . . . . . . c c c c c 6 6 6 6 6 . . . . . . . . . . . 
+. . . . . . . . c c c 4 4 4 4 4 4 4 4 4 4 6 6 6 . . . . . . . . 
+. . . . . . c c 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 6 6 . . . . . . 
+. . . . . c b 4 4 4 4 b b 4 4 4 4 b 5 b 4 4 4 4 4 4 b . . . . . 
+. . . . e b 4 4 4 4 b 5 b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 b . . . . 
+. . . e b b 4 4 4 4 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 6 . . . 
+. . e b 6 b b 4 4 4 4 4 4 4 4 4 b b 4 4 4 b 5 b 4 4 4 4 4 6 . . 
+. . e 6 b b 5 b 4 4 4 4 4 4 4 4 b 5 b 4 4 4 b 4 4 b b 4 4 e . . 
+. e 6 6 b 4 b 4 4 4 4 4 4 4 4 4 4 b 4 4 4 4 4 4 4 b 5 b 4 4 e . 
+. e 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 e . 
+e b 6 6 b 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b e 
+e b 6 6 b b 4 4 4 b 5 b 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 4 4 b e 
+f b 6 6 6 b 4 4 4 b b 4 4 4 4 4 4 4 4 4 b 5 b 4 4 4 4 4 4 4 b f 
+f c b 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b b 4 4 4 4 4 4 b c f 
+. f b b 6 6 6 6 b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f . 
+. e f b b 6 6 6 6 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 b f e . 
+. 8 6 f c b b 6 6 6 6 6 b b b 4 4 4 4 4 4 4 4 4 4 4 b c c 6 8 8 
+8 7 7 2 e f f c b b b b b b b b b b b b b b b b c f c 2 2 7 7 8 
+8 7 7 2 2 2 2 2 c c c c c c c c c c c c c c c c 2 2 2 2 6 6 7 8 
+f 8 6 6 6 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 7 6 6 8 6 f 
+f e f 8 6 6 6 7 7 7 6 6 6 6 7 7 7 7 7 7 6 6 6 7 7 7 7 f f f e f 
+f b f f 8 7 7 7 6 8 f 8 6 7 7 7 7 7 7 6 6 6 7 7 6 f f f f f b f 
+f b e f f e e f f f e f f 7 7 6 6 6 8 8 e f f e e e e f e b 6 f 
+f 6 b f f f e f f e e e e e e e e e e e e e f e e e e e b b 6 e 
+f 6 6 d d f f f f f e e e f f e f f e e e e e f f e e d b 4 6 e 
+. c 6 6 d d d 4 e f f f f f f e e e e e f f f f 4 d d b 4 6 e . 
+. f c 6 b 4 d d d d d d d d d d d d d d d d d d d b 4 4 4 e e . 
+. . f f 6 b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . 
+. . . . f f b b b b 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e e . . . . 
+. . . . . . f f e b b b b b b 4 4 4 4 4 4 4 4 e e e . . . . . . 
+. . . . . . . . . f f f f f f f c c c c c e e . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+Enemy2.vx = 130
+Enemy2.vy = 15
+Enemy2.setFlag(SpriteFlag.BounceOnWall, true)
+mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . b 5 5 b . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. b b b b b 5 5 5 5 5 5 5 b . . 
+. b d 5 b 5 5 5 5 5 5 5 5 b . . 
+. . b 5 5 b 5 d 1 f 5 d 4 f . . 
+. . b d 5 5 b 1 f f 5 4 4 c . . 
+b b d b 5 5 5 d f b 4 4 4 4 b . 
+b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+c d d d c c b 5 5 5 5 5 5 5 b . 
+c b d d d d d 5 5 5 5 5 5 5 b . 
+. c d d d d d d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+`, SpriteKind.Player)
+info.setLife(5)
+mySprite3.setPosition(10, 23)
+controller.moveSprite(mySprite3, 110, 110)
+mySprite3.setFlag(SpriteFlag.StayInScreen, true)
+mySprite4 = sprites.create(img`
+. . . . . . . 6 . . . . . . . . 
+. . . . . . 8 6 6 . . . 6 8 . . 
+. . . e e e 8 8 6 6 . 6 7 8 . . 
+. . e 2 2 2 2 e 8 6 6 7 6 . . . 
+. e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+. e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+e e 2 e 2 2 4 2 2 e e e c . . . 
+e e e e 2 e 2 2 e e e c . . . . 
+e e e 2 e e c e c c c . . . . . 
+. c c c c c c c . . . . . . . . 
+`, SpriteKind.Food)
+mySprite4.setPosition(Math.randomRange(10, 150), Math.randomRange(10, 110))
+let list = [1, 2]
+game.onUpdate(function () {
+    if (info.score() > 4) {
+        if (enemyGenFlag == 0) {
+            Enemy3 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Enemy)
+            Enemy3.vx = Math.randomRange(20, 40)
+            Enemy3.vy = Math.randomRange(50, 70)
+            Enemy3.setFlag(SpriteFlag.BounceOnWall, true)
+            enemyGenFlag = 1
+        }
+    }
+})
+forever(function () {
+    console.logValue("x", controller.acceleration(ControllerDimension.X))
+    console.logValue("y", controller.acceleration(ControllerDimension.Y))
+    mySprite3.setVelocity(Math.map(controller.acceleration(ControllerDimension.X), -30, 30, 0, 60), Math.map(controller.acceleration(ControllerDimension.Y), -500, -400, 120, 0))
+})
